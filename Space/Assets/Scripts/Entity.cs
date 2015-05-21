bdd0c48f4c5a7808042f9a30cafae66891317678
SeruK <strandsebastian2000@gmail.com>
2015-05-21 20:13:21 +0200

    First commit

---
+++ Space/Assets/Scripts/Entity.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+	public float Gravity = -25f;
+	public float RunSpeed = 8f;
+	public float GroundDamping = 20f; // how fast do we change direction? higher means faster
+	public float InAirDamping = 5f;
+	public float JumpHeight = 3f;
+	public float DuckHeight = 0.2f;
+
+	public string IdleAnimationName;
+	public string WalkAnimationName;
+	public string JumpAnimationName;
+	public string HoverAnimationName;
+
+	[HideInInspector]
+	public float RequestedHorizontalSpeed;
+	[HideInInspector]
+	public bool  RequestedJump;
+	[HideInInspector]
+	public bool  RequestedDuck;
+
+	private bool didJump;
+
+	private CharacterController2D charController;
+	private Animator animator;
+	private Transform spriteTransform;
+
+	protected void OnEnable() {
+		if (charController == null) {
+			charController = GetComponentInChildren<CharacterController2D>();
+		}
+		if (animator == null) {
+			animator = GetComponentInChildren<Animator>();
+		}
+		if( spriteTransform == null ) {
+			var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+			if( spriteRenderer != null ) {
+				spriteTransform = spriteRenderer.transform;
+			}
+		}
+	}
+
+	public void Update() {
+		if( charController == null || animator == null ) {
+			return;
+		}
+		
+		// grab our current _velocity to use as a base for all calculations
+		Vector3 velocity = charController.velocity;
+		
+		if( charController.isGrounded ) {
+			didJump = false;
+			velocity.y = 0;
+		}
+		
+		float normalizedHorizontalSpeed = 0;
+
+		if( Mathf.Approximately( RequestedHorizontalSpeed, 0.0f ) ) {
+			normalizedHorizontalSpeed = 0;
+			if( charController.isGrounded )
+				animator.Play( Animator.StringToHash( IdleAnimationName ) );
+		} else if( RequestedHorizontalSpeed > 0.0f ) {
+			normalizedHorizontalSpeed = 1;
+			if( spriteTransform.localScale.x < 0f )
+				spriteTransform.localScale = new Vector3( -spriteTransform.localScale.x, spriteTransform.localScale.y, spriteTransform.localScale.z );
+			
+			if( charController.isGrounded )
+				animator.Play( Animator.StringToHash( WalkAnimationName ) );
+		} else {
+			normalizedHorizontalSpeed = -1;
+			if( spriteTransform.localScale.x > 0f )
+				spriteTransform.localScale = new Vector3( -spriteTransform.localScale.x, spriteTransform.localScale.y, spriteTransform.localScale.z );
+			
+			if( charController.isGrounded )
+				animator.Play( Animator.StringToHash( WalkAnimationName ) );
+		}
+
+		float scaleY = 1.0f;
+
+		// we can only jump whilst grounded
+		if( charController.isGrounded ) {
+			if( RequestedJump ) {
+				velocity.y = Mathf.Sqrt( 2f * JumpHeight * -Gravity );
+				didJump = true;
+			} else if( RequestedDuck ) {
+				scaleY = 1.0f - DuckHeight;
+			}
+		} /* else if( !RequestedJump && velocity.y > 0.0f ) {
+			velocity.y = 0.0f;
+		} */
+		scaleY = Mathf.Lerp( spriteTransform.localScale.x, scaleY, 0.01f );
+		spriteTransform.localScale = new Vector3( spriteTransform.localScale.x, scaleY, spriteTransform.localScale.z );
+		
+
+		// apply horizontal speed smoothing it
+		var smoothedMovementFactor = charController.isGrounded ? GroundDamping : InAirDamping; // how fast do we change direction?
+		velocity.x = Mathf.Lerp( velocity.x, normalizedHorizontalSpeed * RunSpeed, Time.deltaTime * smoothedMovementFactor );
+		
+		// apply gravity before moving
+		velocity.y += Gravity * Time.deltaTime;
+		
+		charController.move( velocity * Time.deltaTime );
+
+		if( !charController.isGrounded ) {
+			string hover = string.IsNullOrEmpty( HoverAnimationName ) ? JumpAnimationName : HoverAnimationName;
+			string anim  = hover;
+			if( velocity.y > 0.0f && didJump ) {
+				anim = JumpAnimationName;
+			}
+			animator.Play( Animator.StringToHash( anim ) );
+		}
+	}
+}
---
+++ Space/Assets/Scripts/Game.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+	public Entity Player;
+	public SmoothFollow CameraController;
+
+	public GUIText  DisplayText;
+	public float    DisplayTextWait;
+	public Material DisplayTextGlitchMaterial;
+	public float    DisplayTextGlitchDuration;
+	public Vector2  DisplayTextGlitchRandom;
+	public float    DisplayTextGlitchOffset;
+
+	public string TextToDisplay;
+	private string lastTextToDisplay;
+	private int currentTextLength;
+	private float nextTextCharIn;
+	private Material displayTextNormalMaterial;
+	private float nextTextGlitchIn;
+	private float currentTextGlitchTimer;
+	private Vector2 currentTextGlitchOffset;
+
+	protected void OnEnable() {
+		if( DisplayText != null ) {
+			displayTextNormalMaterial = DisplayText.material;
+		}
+	}
+
+	protected void Update() {
+		InputPlayer();
+		UpdateDisplayText();
+	}
+
+	protected void InputPlayer() {
+		if( Player == null ) {
+			return;
+		}
+
+		Player.RequestedHorizontalSpeed = Input.GetKey( KeyCode.LeftArrow ) ? -1.0f :
+			Input.GetKey( KeyCode.RightArrow ) ? 1.0f : 0.0f;
+		Player.RequestedJump = Input.GetKey( KeyCode.UpArrow );
+		if( Input.GetKey( KeyCode.DownArrow ) && CameraController != null ) {
+			CameraController.extraCameraOffset = new Vector3( 0.0f, 2.0f, 0.0f );
+		} else {
+			CameraController.extraCameraOffset = Vector3.zero;
+		}
+	}
+
+	protected void UpdateDisplayText() {
+		if( DisplayText == null ) {
+			return;
+		}
+
+		DisplayText.pixelOffset = new Vector2( Screen.width / 2.0f, Screen.height - 40.0f ) + currentTextGlitchOffset;
+		
+		if( lastTextToDisplay != TextToDisplay ) {
+			currentTextLength = 0;
+		}
+		
+		lastTextToDisplay = TextToDisplay;
+
+		if( DisplayTextGlitchMaterial != null ) {
+			if( currentTextGlitchTimer > 0.0f ) {
+				currentTextGlitchTimer -= Time.deltaTime;
+
+				// Reset to normal
+				if( currentTextGlitchTimer < 0.0f ) {
+					DisplayText.material = displayTextNormalMaterial;
+					currentTextGlitchOffset = Vector2.zero;
+				}
+			} else {
+				DisplayText.material = displayTextNormalMaterial;
+
+				if( nextTextGlitchIn <= 0.0f ) {
+					nextTextGlitchIn = Random.Range( DisplayTextGlitchRandom.x, DisplayTextGlitchRandom.y );
+				}
+
+				if( nextTextGlitchIn > 0.0f ) {
+					nextTextGlitchIn -= Time.deltaTime;
+
+					if( nextTextGlitchIn < 0.0f ) {
+						currentTextGlitchTimer = DisplayTextGlitchDuration;
+						DisplayText.material = DisplayTextGlitchMaterial;
+						currentTextGlitchOffset = Random.insideUnitCircle * DisplayTextGlitchOffset;
+					}
+				}
+			}
+		} else {
+			DisplayText.material = displayTextNormalMaterial;
+		}
+		
+		if( string.IsNullOrEmpty( TextToDisplay ) ) {
+			DisplayText.text = "";
+		} else {
+			if( currentTextLength < TextToDisplay.Length ) {
+				if( nextTextCharIn > 0.0f ) {
+					nextTextCharIn -= Time.deltaTime;
+				}
+				
+				if( nextTextCharIn <= 0.0f ) {
+					do {
+						++currentTextLength;
+						char c = TextToDisplay[ currentTextLength - 1 ];
+						if( !char.IsWhiteSpace( c )) {
+							break;
+						}
+					} while ( currentTextLength <= TextToDisplay.Length );
+					nextTextCharIn = Mathf.Max( 0.0f, DisplayTextWait );
+					DisplayText.text = TextToDisplay.Substring( 0, currentTextLength );
+				}
+			}
+		}
+	}
+}
