380cdcdebab3bf67c6d0460271fc4226953e8837
SeruK <strandsebastian2000@gmail.com>
2015-05-29 16:16:21 +0200

    More TileMap work + Size2 class

--- Space/Assets/Scripts/System/TileMap.cs
+++ Space/Assets/Scripts/System/TileMap.cs
@@ -1,25 +1,43 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 // Modeled after TMX-format
 // http://doc.mapeditor.org/reference/tmx-map-format/#imagelayer
 
 namespace SA {
 	public class TileMapProperty {
-		
+
 	}
 
 	public class TileMapObject {
-		
+		private uint    id;
+		private string  name;
+		private Rect    bounds;
+		//private float   rotation;
+		//private uint    gid;
+		private bool    visible;
+
+		public Rect Bounds {
+			get { return bounds; }
+		}
+
+		public Vector2 Position {
+			get { return bounds.position; }
+		}
+
+		public Size2 Size {
+			get { return (Size2)bounds.size; }
+		}
 	}
 
 	public class TileMapObjectGroup {
-
+		private TileMapProperty[] properties;
+		private TileMapObject[]   objects;
 	}
 
 	public class Tileset {
 		// The global ID that maps to the first tile
 		// in this tileset
-		private int    firstGID;
+		private uint firstGID;
 
 		// External tilesets
 		//private string source
@@ -31,7 +49,7 @@ namespace SA {
 		// Offset in pixels
 		private Vector2i tileOffset;
 
-		public int FirstGID {
+		public uint FirstGID {
 			get { return firstGID; }
 		}
 
@@ -45,13 +63,39 @@ namespace SA {
 	}
 
 	public class TileLayer {
+		// Name of the layer
+		private string name;
+		// Opacity of layer 
+		private float  opacity;
+		// Is layer shown or hidden
+		private bool   visible;
+		// Tile data
+		private System.UInt32[] tiles;
 
+		public string Name {
+			get { return name; }
+		}
+		public float Opacity {
+			get { return opacity; }
+		}
+		public bool Visible {
+			get { return visible; }
+		}
 	}
 
 	public class TileMap {
+		// Id
+		private int    id;
+		// Size of the map in tiles
 		private Size2i size;
+		// Size of each tile in pixels
 		private Size2i tileSize;
+		// Color drawn behind tiles
 		private Color  backgroundColor;
+
+		private TileLayer[]          layers;
+		private TileMapProperty[]    properties;
+		private TileMapObjectGroup[] objectGroups;
 
 		public Size2i Size {
 			get { return size; }
@@ -61,6 +105,9 @@ namespace SA {
 		}
 		public Color BackgroundColor {
 			get { return backgroundColor; }
+		}
+		public TileLayer[] Layers {
+			get { return layers; }
 		}
 	}
 }
