c0181b5dbe38d7df6793e2cd002f69a69afeb78f
SeruK <strandsebastian2000@gmail.com>
2015-06-16 19:51:52 +0200

    XElement parsing WIP

--- Space/Assets/Scripts/System/TileMap.cs
+++ Space/Assets/Scripts/System/TileMap.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Xml;
+using System.Xml.Linq;
 using System.IO;
 using Ionic.Zlib;
 
@@ -75,6 +76,7 @@ namespace SA {
 		private Size2i tileSize;
 		// Relative image path (from .tmx or .tsx file)
 		private string imagePath;
+		private TileMapProperty[] properties;
 
 		public string Name {
 			get { return name; }
@@ -87,6 +89,17 @@ namespace SA {
 		}
 		public string ImagePath {
 			get { return imagePath; }
+		}
+		public TileMapProperty[] Properties {
+			get { return properties; }
+		}
+
+		public Tileset( string name, uint firstGID, Size2i tileSize, string imagePath, TileMapProperty[] properties ) {
+			this.name = name;
+			this.firstGID = firstGID;
+			this.tileSize = tileSize;
+			this.imagePath = imagePath;
+			this.properties = properties;
 		}
 	}
 
@@ -171,12 +184,82 @@ namespace SA {
 			get { return objectLayers; }
 		}
 
-		private TileMap() {
+		public TileMap( Size2i size, Size2i tileSize, Color bgColor,
+		                Tileset[] tilesets, TileMapProperty[] properties,
+		                TileLayer[] tileLayers, ObjectLayer[] objectLayers ) {
+			this.size            = size;
+			this.tileSize        = tileSize;
+			this.backgroundColor = bgColor;
+			this.tilesets        = tilesets;
+			this.properties      = properties;
+			this.tileLayers      = tileLayers;
+			this.objectLayers    = objectLayers;
 		}
 	}
 
 	public static class TileMapTMXReader {
 		public static TileMap ParseTMXFileAtPath( string filePath ) {
+			using( XmlReader reader = XmlReader.Create( new StreamReader( filePath ) ) ) {
+				XElement map = XElement.Load( reader );
+				return ParseTileMap( map );
+			}
+
+			return null;
+		}
+
+		private static TileMap ParseTileMap( XElement map ) {
+			// Assume orthogonal orientation and right-down renderorder
+			var size = new Size2i( (int)map.Attribute( "width" ), (int)map.Attribute( "height" ) );
+			var tileSize = new Size2i( (int)map.Attribute( "tilewidth" ), (int)map.Attribute( "tileheight" ) );
+			
+			var bgColor = Color.white;
+			var bgColorElement = map.Attribute( "backgroundcolor" );
+			if( bgColorElement != null ) {
+				bgColor = ParseColorString( bgColorElement.Value, bgColor );
+			}
+
+			var tilesets = new List<Tileset>();
+			TileMapProperty[] properties;
+			var tileLayers = new List<TileLayer>();
+			var objectLayers = new List<ObjectLayer>();
+
+			foreach( var child in map.Elements() ) {
+				if( child.Name == "tileset" ) {
+					tilesets.Add( ParseTileset( child ) );
+				} else if( child.Name == "properties" ) {
+					properties = ParseProperties( child );
+				}
+			}
+
+			return null;
+		}
+
+		private static Tileset ParseTileset( XElement tileset) {
+			string externalSource = tileset.Attribute( "source" ).Value;
+
+			if( !string.IsNullOrEmpty( externalSource ) ) {
+				Debug.Log( "External tileset" );
+				return null;
+			}
+
+			string name = tileset.Attribute( "name" ).Value;
+			uint firstGID = (uint)tileset.Attribute( "firstGID" );
+
+			int w = (int) tileset.Attribute( "tilewidth" );
+			int h = (int) tileset.Attribute( "tileheight" );
+			var tileSize = new Size2i( w, h );
+
+			string imagePath = tileset.Element( "image ").Attribute( "source" ).Value;
+
+			var properties = ParseProperties( tileset.Element( "properties" ) );
+
+			return new Tileset( name, firstGID, tileSize, imagePath, properties );
+		}
+
+		private static TileMapProperty[] ParseProperties( XElement properties ) {
+			if( properties == null ) {
+				return new TileMapProperty[ 0 ];
+			}
 			return null;
 		}
 
@@ -187,10 +270,16 @@ namespace SA {
 
 			// Could be done much faster by converting to int and bitshifting
 			// Format: #ffaaff
-			var red   = System.Convert.ToUInt16( colorString.Substring( 1, 2 ), 16 );
-			var green = System.Convert.ToUInt16( colorString.Substring( 3, 2 ), 16 );
-			var blue  = System.Convert.ToUInt16( colorString.Substring( 5, 2 ), 16 );
-			
+			System.UInt16 red, green, blue;
+			try {
+				red   = System.Convert.ToUInt16( colorString.Substring( 1, 2 ), 16 );
+				green = System.Convert.ToUInt16( colorString.Substring( 3, 2 ), 16 );
+				blue  = System.Convert.ToUInt16( colorString.Substring( 5, 2 ), 16 );
+			} catch( System.Exception e ) {
+				DebugUtil.LogError( "Invalid color string: " + colorString );
+				return def;
+			}
+
 			return new Color( red / 255.0f, green / 255.0f, blue / 255.0f );
 		}
 
