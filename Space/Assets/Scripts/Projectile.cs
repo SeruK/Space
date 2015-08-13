19fd343125a8ee0b2087051dbf1dcdc8d5fe8b9d
SeruK <strandsebastian2000@gmail.com>
2015-08-13 13:19:08 +0200

    * Projectile WIP

---
+++ Space/Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent( typeof(Rigidbody2D) )]
+public class Projectile : BaseEntity {
+	[SerializeField]
+	private float lifeTime;
+	[SerializeField]
+	private float speed;
+
+	public System.Action<Projectile> OnDeath;
+
+	private float lifeTimer;
+	private float currentSpeed;
+
+	public void ShootTowards( Vector2 point ) {
+		Vector2 v = point - (Vector2)transform.position;
+		float angle = Mathf.Atan2( v.y, v.x ) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler( 0.0f, 0.0f, angle );
+		currentSpeed = speed;
+		lifeTimer = lifeTime;
+	}
+
+	protected void Update() {
+		transform.position += transform.TransformDirection( Vector3.forward ) * currentSpeed;
+
+		lifeTimer -= lifeTime;
+		if( lifeTimer < 0.0f && OnDeath != null ) {
+			OnDeath( this );
+		}
+	}
+}
